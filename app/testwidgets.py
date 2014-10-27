f85633c86d99f9bef1ec0634728cbf7cdbfc08f9
Padavan <padavankun@gmail.com>
2014-10-27 15:31:36 +0300

    sound boolean blahlbalh

--- app/testwidgets.py
+++ app/testwidgets.py
@@ -25,14 +25,14 @@ class PrefWindow(QtGui.QWidget):
 #--------Tab 1-----------------------------------------------------------------
  #-------смотреть здесь------------
         self.ex=Config()
+        #self.ex.write()
         self.ex.read()
-        self.pomodoro=self.ex.pomodoro
 
         self.label1=QtGui.QLabel("Pomodoro Time", self.tab1)
         self.slider1=QtGui.QSlider(QtCore.Qt.Horizontal, self.tab1)
-        self.slider1.setValue(self.pomodoro)
         self.sbox1=QtGui.QSpinBox(self.tab1)
-        self.sbox1.setValue(self.pomodoro)
+        self.slider1.setValue(self.ex.pomodoro)
+        self.sbox1.setValue(self.ex.pomodoro)
         self.slider1.valueChanged.connect(self.sbox1.setValue)
         self.sbox1.valueChanged.connect(self.slider1.setValue)
 #-------/смотреть здесь------------
@@ -44,6 +44,8 @@ class PrefWindow(QtGui.QWidget):
         self.label2=QtGui.QLabel("Rest", self.tab1)
         self.slider2=QtGui.QSlider(QtCore.Qt.Horizontal, self.tab1)
         self.sbox2=QtGui.QSpinBox(self.tab1)
+        self.slider2.setValue(self.ex.rest)
+        self.sbox2.setValue(self.ex.rest)
         self.slider2.valueChanged.connect(self.sbox2.setValue)
         self.sbox2.valueChanged.connect(self.slider2.setValue)
         self.hbox2=QtGui.QHBoxLayout()
@@ -54,6 +56,8 @@ class PrefWindow(QtGui.QWidget):
         self.label3=QtGui.QLabel("Long Rest", self.tab1)
         self.slider3=QtGui.QSlider(QtCore.Qt.Horizontal, self.tab1)
         self.sbox3=QtGui.QSpinBox(self.tab1)
+        self.slider3.setValue(self.ex.lrest)
+        self.sbox3.setValue(self.ex.lrest)
         self.slider3.valueChanged.connect(self.sbox3.setValue)
         self.sbox3.valueChanged.connect(self.slider3.setValue)
         self.hbox3=QtGui.QHBoxLayout()
@@ -61,17 +65,19 @@ class PrefWindow(QtGui.QWidget):
         self.hbox3.addWidget(self.slider3)
         self.hbox3.addWidget(self.sbox3)
 
-        self.chbox4=QtGui.QCheckBox()
-        self.label4=QtGui.QLabel("Ticking Sound")
-        self.hbox4=QtGui.QHBoxLayout()
-        self.hbox4.addWidget(self.chbox4)
-        self.hbox4.addWidget(self.label4)
+        self.chbox4=QtGui.QCheckBox('Ticking Sound')
+        self.chbox4.setChecked(self.ex.sound)
+        #print self.chbox4.isChecked()
+        #self.label4=QtGui.QLabel("Ticking Sound")
+        #self.hbox4=QtGui.QHBoxLayout()
+        #self.hbox4.addWidget(self.chbox4)
+        #self.hbox4.addWidget(self.label4)
 
         self.bigVBox=QtGui.QVBoxLayout()
         self.bigVBox.addLayout(self.hbox1)
         self.bigVBox.addLayout(self.hbox2)
         self.bigVBox.addLayout(self.hbox3)
-        self.bigVBox.addLayout(self.hbox4)
+        self.bigVBox.addWidget(self.chbox4)
         self.tab1.setLayout(self.bigVBox)
 
 
@@ -105,8 +111,13 @@ class PrefWindow(QtGui.QWidget):
         self.mainWidget.show()
 #-------эта штука для проверки значений------------
     def printValues(self):
-        self.pomodoro = self.slider1.value()
-        print self.pomodoro
+        self.ex.pomodoro = self.slider1.value()
+        self.ex.rest = self.slider2.value()
+        self.ex.lrest = self.slider3.value()
+        self.ex.sound = self.chbox4.isChecked()
+        print self.ex.pomodoro, self.ex.rest, self.ex.lrest
+        print self.ex.sound
+        self.ex.write()
 #-----здесь будет ex.write()
 
 
@@ -119,24 +130,32 @@ class Config(object):
 
     def __init__(self):
         self.config=ConfigParser.ConfigParser()
+        self.pomodoro = 0
+        self.rest = 0
+        self.lrest = 0
 
     def read(self):
         #self.cfgfile=open('config.ini')
         self.config.read('config.ini')
-        print self.config.sections()
+        #print self.config.sections()
         self.pomodoro=self.config.getint('Timer', 'Pomodoro')
-        print "configvalue %d" % self.pomodoro
+        #print "configvalue %d" % self.pomodoro
         self.rest=self.config.getint('Timer', 'Rest')
         self.lrest=self.config.getint('Timer', 'Long Rest')
+        self.sound=self.config.getboolean('Timer', 'sound')
+        print self.sound
         #self.cfgfile.close()
     def write(self):
+
         self.cfgfile=open('config.ini', 'w')
 
+        #self.config.add_section('Timer')
         self.config.set('Timer', 'Pomodoro', self.pomodoro)
         self.config.set('Timer', 'Rest', self.rest)
         self.config.set('Timer', 'Long Rest', self.lrest)
+        self.config.set('Timer', 'sound', self.sound)
 
-        self.config.write(cfgfile)
+        self.config.write(self.cfgfile)
         self.cfgfile.close()
     def update(self):
         pass
